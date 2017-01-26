53f9f9b06e27ff4bbb0e1da460170cc302da00ea
Pekka Alli <pekkaalli@Pekkas-MacBook-Pro.local>
2017-01-26 18:58:00 +0200

    Added description and cleaned main.c file

--- Readme.md
+++ Readme.md
@@ -1,1 +1,10 @@
-Simpel parser
+Simple tokenizer. Built to replace 3.rd party parser from https://github.com/allip1/allisp
+
+#To run demo
+
+'''
+make
+./main
+'''
+
+
--- main.c
+++ main.c
@@ -5,30 +5,13 @@
 
 
 int main() {
-
-    token* t = token_create_string("test token", 0, 1);
-    token* b = token_create_int(1, 1, 2);
-    token* a = token_create_float(1.3 , 4, 5);
-    token* c = token_create_name("test name", 4, 5);
-    token* d = token_create_operator("unary", 6, 6);
-/*
-    token_print(t);
-    token_print(b);
-    token_print(a);
-    token_print(c);
-    token_print(d);
-*/
-    token_free(a);
-    token_free(b);
-    token_free(c);
-    token_free(d);
-    token_free(t);
+    char* input = "int main(){ int a= 0; a++; return(0); }";
     
-    printf("\n");
-    token_list* toks = tokenize("int main(){ int a= 0; a++; return(0); }");
+    printf("tokenizing %s \n", input);
+
+    token_list* toks = tokenize(input);
     print_token_list(toks);
     token_list_free(toks);
     return 1;
-
 }
 
